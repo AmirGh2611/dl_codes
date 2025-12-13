80d609aac3b8f879ad3d0ac8df12bffe6e57e920
amirhossein <amirabbasghorbani354@gmail.com>
2025-12-14 00:19:07 +0330

    create mlp model

---
+++ persian_number/keras_imp/keras_model.py
@@ -0,0 +1,8 @@
+from keras.models import Sequential
+from keras.layers import Dense, Input
+
+model = Sequential([
+    Input(shape=(25,)),
+    Dense(64, activation="relu"),
+    Dense(10, activation="softmax"),
+])
