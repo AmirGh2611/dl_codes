ff78740215edd7436510926366f8d50c4cdf1329
amirhossein <amirabbasghorbani354@gmail.com>
2025-12-14 00:26:21 +0330

    change

--- persian_number/keras_imp/training.py
+++ persian_number/keras_imp/training.py
@@ -5,15 +5,17 @@ import matplotlib.pyplot as plt
 model.compile(loss="categorical_crossentropy",
               optimizer="rmsprop",
               metrics=["accuracy"])
+
 history = model.fit(x_train, y_train,
                     epochs=50,
                     batch_size=256,
                     validation_split=0.2)
+
 plt.figure(figsize=(10, 5))
-plt.plot(history.history["accuracy"])
-plt.plot(history.history["val_accuracy"])
-plt.title("Model accuracy")
-plt.ylabel("Accuracy")
-plt.xlabel("Epoch")
-plt.legend(["train", "validation"], loc="upper right")
+plt.plot(history.history["loss"])
+plt.plot(history.history["val_loss"])
+plt.title("Loss")
+plt.ylabel("loss")
+plt.xlabel("epoch")
+plt.legend(["train", "validation"])
 plt.show()
