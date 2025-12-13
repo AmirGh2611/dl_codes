e2b866dc58e1a83cdf7dd0e08f2c57381d4a2a49
amirhossein <amirabbasghorbani354@gmail.com>
2025-12-14 00:26:14 +0330

    improve the model

--- persian_number/keras_imp/keras_model.py
+++ persian_number/keras_imp/keras_model.py
@@ -3,6 +3,8 @@ from keras.layers import Dense, Input
 
 model = Sequential([
     Input(shape=(25,)),
+    Dense(32, activation="relu"),
     Dense(64, activation="relu"),
+    Dense(128, activation="relu"),
     Dense(10, activation="softmax"),
 ])
